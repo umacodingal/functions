8b7356435a6931154eb1ec528c8fec889b3a6f7e
umacodingal <uma.codingal@gmail.com>
2025-02-02 19:20:10 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+def intro():
+  print("welcome arham,welcome areiz")
+intro()
+#method2
+def add(number1,number2):
+  result=number1+number2
+  print("the answer is: ",result)
+  
+def sub(number1,number2):
+  result=number1-number2
+  print("the answer is: ",result)
+
+
+number1=int(input("enter number1: ") )
+number2=int(input("enter number2: ") )
+#function calling
+add(number1,number2)
+sub(number1,number2)
+
